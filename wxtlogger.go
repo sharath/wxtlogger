156740ddd561cc5178d356aa8d20ad404c5b66ea
sharath <sharathramku@umass.edu>
2017-12-06 14:10:49 -0500

    loading from config complete

--- wxtlogger.go
+++ wxtlogger.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"github.com/tarm/serial"
+	wxt "wxtlogger/WeatherStation"
+	conf "wxtlogger/Config"
 	"time"
 )
 
+func sample(x *wxt.WeatherStation) {
+	for {
+		x.UpdateResponse()
+		time.Sleep(time.Second)
+	}
+}
+
 
 func main() {
-	fmt.Println("starting wxtlogger")
-	c := &serial.Config{Name: "/dev/ttyUSB1", Baud: 4800}
-	s, _ := serial.OpenPort(c)
-	time.Sleep(2)
-	_, _ = s.Write([]byte("0R0\r\n"))
-	time.Sleep(2)
-	buf := make([]byte, 128)
-	_, _ = s.Read(buf)
-	time.Sleep(2)
-	fmt.Println(string(buf))
+	wstations := conf.Load("config.json").Wxt;
+	for _, w := range wstations {
+		w.Configure()
+		go sample(&w)
+	}
+	select {}
 }
