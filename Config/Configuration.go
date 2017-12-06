98b9ac47030ddf3abedff0bdcbe7621d8bbd59e3
sharath <sharathramku@umass.edu>
2017-12-06 14:11:36 -0500

    file loading and unmarshal complete

--- Config/Configuration.go
+++ Config/Configuration.go
@@ -1,5 +1,18 @@
 package Config
 
-type Configuration struct {
+import (
+	"wxtlogger/WeatherStation"
+	"io/ioutil"
+	"encoding/json"
+)
 
+type Configuration struct {
+	Wxt []WeatherStation.WeatherStation
+}
+
+func Load(file string) *Configuration {
+	cf := new(Configuration)
+	configFile,_ := ioutil.ReadFile(file)
+	json.Unmarshal(configFile, &cf.Wxt)
+	return cf
 }
