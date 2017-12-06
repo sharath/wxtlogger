6cf946d728ac222c91ee3743df27d9a78f6bf90f
sharath <sharathramku@umass.edu>
2017-12-06 13:39:11 -0500

    correctly parsing response into struct

--- WeatherStation/WXTResponse.go
+++ WeatherStation/WXTResponse.go
@@ -1,6 +1,14 @@
 package WeatherStation
 
+import (
+	"time"
+	"strings"
+	"strconv"
+	"fmt"
+)
+
 type WXTResponse struct {
+	Time time.Time
 	WindUnits string
 	PressureUnits string
 	TempUnits string
@@ -9,4 +17,42 @@ type WXTResponse struct {
 	Temp float64
 	Humidity float64
 	Pressure float64
+}
+
+func cut(str string, start string, end string) string {
+	t := []rune(str)
+	t = t[len(start):len(str)-len(end)]
+	println(string(t))
+	return string(t)
+}
+
+func (storage *WXTResponse) Parse(resp []string) {
+	for _, p := range resp {
+		t := p
+		for strings.Contains(t, "\x00") || strings.Contains(t, "\x00") {
+			t = strings.TrimSuffix(t, "\x00")
+			t = strings.TrimSuffix(t, "\r\n")
+		}
+		if strings.Contains(t, "Sm=") {
+			storage.WindAvg, _ = strconv.ParseFloat(cut(t, "Sm=", "M"), 64)
+			continue
+		}
+		if strings.Contains(t, "Dm=") {
+			storage.WindDir, _ = strconv.Atoi(cut(t, "Dm=", "D"))
+			continue
+		}
+		if strings.Contains(t, "Ta=") {
+			storage.Temp, _ = strconv.ParseFloat(cut(t, "Ta=", "F"), 64)
+			continue
+		}
+		if strings.Contains(t, "Ua=") {
+			storage.Humidity, _ = strconv.ParseFloat(cut(t, "Ua=", "P"), 64)
+			continue
+		}
+		if strings.Contains(t, "Pa=") {
+			storage.Pressure, _ = strconv.ParseFloat(cut(t, "Pa=", "H"), 64)
+			continue
+		}
+	}
+	fmt.Println(storage)
 }
--- WeatherStation/WeatherStation.go
+++ WeatherStation/WeatherStation.go
@@ -3,6 +3,7 @@ package WeatherStation
 import (
 	"github.com/tarm/serial"
 	"time"
+	"strings"
 )
 
 type WeatherStation struct {
@@ -24,31 +25,54 @@ func (wxt *WeatherStation) write(command string) {
 	wxt.port.Write([]byte(command))
 }
 
-func (wxt *WeatherStation) Read() string {
+func (wxt *WeatherStation) read() string {
 	buf := make([]byte, 1024)
 	wxt.port.Read(buf)
 	return string(buf)
 }
 
 func (wxt *WeatherStation) Configure() {
+	delay := time.Millisecond * 300
+
+	wxt.port.Flush()
+	// send set_comm
+	wxt.write("0XU,M=P,C=3,B=4800,L=25\r\n")
+	time.Sleep(delay)
+	// send set_wind_conf
+	wxt.write("0WU,I=1,A=1,U=M,D=0,N=W,F=1\r\n")
+	time.Sleep(delay)
+	// send set_wind_parameters
+	wxt.write("0WU,R=0100100001001000\r\n")
+	time.Sleep(delay)
+	// send set_ptu_conf
+	wxt.write("0TU,I=1,P=H,T=F\r\n")
+	time.Sleep(delay)
+	// send set_ptu_parameters
+	wxt.write("0TU,R=1101000011010000\r\n")
+	time.Sleep(delay)
+	// send set_super_conf
+	wxt.write("0SU,S=N,H=Y,I=5\r\n")
+	time.Sleep(delay)
+	// send set_super_parameters
+	wxt.write("0SU,R=1111000000000000\r\n")
+	time.Sleep(delay)
+	wxt.port.Flush()
+
 	// set units
 	wxt.Response.WindUnits = "m/s"
 	wxt.Response.PressureUnits = "hPa"
 	wxt.Response.TempUnits = "F"
-	wxt.port.Flush()
-	// send set_comm
-	wxt.write("0XU,M=P,C=3,B=4800,L=25\r\n")
-	// send set_wind_conf
-	wxt.write("0WU,I=1,A=1,U=M,D=0,N=W,F=1\r\n")
-	// send set_wind_parameters
-	wxt.write("0WU,R=0100100001001000\r\n")
-	// send set_ptu_conf
-	wxt.write("0TU,I=1,P=H,T=F\r\n")
-	// send set_ptu_parameters
-	wxt.write("0TU,R=1101000011010000\r\n")
-	// send set_super_conf
-	wxt.write("0SU,S=N,H=Y,I=5\r\n")
-	// send set_super_parameters
-	wxt.write("0SU,R=1111000000000000\r\n")
-	wxt.port.Flush()
+
+	// send 1st sample request
+	wxt.write("0R0\r\n")
+	time.Sleep(time.Second)
+}
+
+
+func (wxt *WeatherStation) UpdateResponse() {
+	resp := strings.Split(strings.TrimSpace(wxt.read()),",")
+	// parse the values
+	wxt.Response.Parse(resp[1:len(resp)])
+	// next sample request
+	wxt.write("0R0\r\n")
 }
