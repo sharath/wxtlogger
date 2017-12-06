0f73df2668a80741afa2b5a8dba53d3e04de8a71
sharath <sharathramku@umass.edu>
2017-12-06 16:27:15 -0500

    file output and splitting each day

--- wxtlogger.go
+++ wxtlogger.go
@@ -4,12 +4,56 @@ import (
 	wxt "wxtlogger/WeatherStation"
 	conf "wxtlogger/Config"
 	"time"
+	"fmt"
+	"os"
+	"path"
+	"bufio"
 )
 
 func sample(x *wxt.WeatherStation, i int) {
+	var index uint64 = 0
+
+	folder := time.Now().Format("data-20060102")
+	filename := fmt.Sprintf("WX%d-%s",i,time.Now().Format("20060102-030405.txt"))
+	os.Mkdir(folder, 0777)
+	file, _ := os.Create(path.Join(folder, filename))
+	w := bufio.NewWriter(file)
+
+	header := fmt.Sprintf("Model Number: WXT-510\n");
+	header += fmt.Sprintf("Sample rate: 1 (Hz)\n");
+	header += fmt.Sprintf("Wind speed units: m/s\n");
+	header += fmt.Sprintf("Pressure units: hPa\n");
+	header += fmt.Sprintf("Temperature units: F\n\n");
+	header += fmt.Sprintf("Index, Hour, Minute, Second, Direction, Speed, Temp, Humidity, Pressure\n");
+	header += fmt.Sprintf("_______________________________________________________________________\n\n");
+	fmt.Print(header)
+	fmt.Fprint(w, header)
+	w.Flush()
+
 	for {
-		x.UpdateResponse()
-		time.Sleep(time.Second)
+		if time.Now().Second() != x.Response.Time.Second() {
+			if time.Now().Day() != x.Response.Time.Day() {
+				file.Close()
+				folder = time.Now().Format("data-20060102")
+				filename = fmt.Sprintf("WX%d-%s",i,time.Now().Format("20060102-030405.txt"))
+				os.Mkdir(folder, 0777)
+				file, _ = os.Create(path.Join(folder, filename))
+				w = bufio.NewWriter(file)
+				fmt.Print(header)
+				fmt.Fprint(w, header)
+				w.Flush()
+			}
+			x.UpdateResponse()
+			line := fmt.Sprintf("%d, %s, %d, %.1f, %.1f, %.1f, %.1f\n",
+				index, x.Response.Time.Format("15, 04, 05.999999"),
+					x.Response.WindDir, x.Response.WindAvg,
+						x.Response.Temp, x.Response.Humidity,
+							x.Response.Pressure)
+			fmt.Print(line)
+			fmt.Fprint(w, line)
+			w.Flush()
+			index++
+		}
 	}
 }
 
