729b191571bc86e3c37dc173d41bdaf7bdbda317
sharath <sharathramku@umass.edu>
2017-12-06 15:06:03 -0500

    cleanup

--- WeatherStation/WeatherStation.go
+++ WeatherStation/WeatherStation.go
@@ -28,35 +28,27 @@ func (wxt *WeatherStation) Configure() {
 	delay := time.Millisecond * 200
 	time.Sleep(delay)
 	wxt.port.Flush()
-	// send set_comm
 	wxt.write("0XU,M=P,C=3,B=4800,L=25\r\n")
 	time.Sleep(delay)
-	// send set_wind_conf
 	wxt.write("0WU,I=1,A=1,U=M,D=0,N=W,F=1\r\n")
 	time.Sleep(delay)
-	// send set_wind_parameters
 	wxt.write("0WU,R=0100100001001000\r\n")
 	time.Sleep(delay)
-	// send set_ptu_conf
 	wxt.write("0TU,I=1,P=H,T=F\r\n")
 	time.Sleep(delay)
-	// send set_ptu_parameters
 	wxt.write("0TU,R=1101000011010000\r\n")
 	time.Sleep(delay)
-	// send set_super_conf
 	wxt.write("0SU,S=N,H=Y,I=5\r\n")
 	time.Sleep(delay)
-	// send set_super_parameters
 	wxt.write("0SU,R=1111000000000000\r\n")
 	time.Sleep(delay)
 	wxt.port.Flush()
 
-	// set units
 	wxt.Response.WindUnits = "m/s"
 	wxt.Response.PressureUnits = "hPa"
 	wxt.Response.TempUnits = "F"
 
-	// send 1st sample request
+	wxt.Response.Time = time.Now()
 	wxt.write("0R0\r\n")
 	time.Sleep(time.Second)
 }
@@ -64,9 +56,7 @@ func (wxt *WeatherStation) Configure() {
 
 func (wxt *WeatherStation) UpdateResponse() {
 	resp := strings.Split(strings.TrimSpace(wxt.read()),",")
-	// parse the values
+	wxt.Response.Parse(resp[1:])
 	wxt.Response.Time = time.Now()
-	wxt.Response.Parse(resp[1:len(resp)])
-	// next sample request
 	wxt.write("0R0\r\n")
 }
