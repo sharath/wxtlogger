4331ffd58692d3b38f81db02e33f447d21e22679
sharath <sharathramku@umass.edu>
2017-12-09 12:25:41 -0500

    cleaner

--- WeatherStation/Device.go
+++ WeatherStation/Device.go
@@ -26,23 +26,13 @@ func (wxt *Device) read() string {
 
 func (wxt *Device) Configure() {
 	wxt.port, _ = serial.OpenPort(&serial.Config{Name: wxt.Location, Baud: wxt.Baud, ReadTimeout: time.Second})
-	delay := time.Millisecond * 200
-	time.Sleep(delay)
-	wxt.port.Flush()
-	wxt.write("0XU,M=P,C=3,B=4800,L=25\r\n")
-	time.Sleep(delay)
-	wxt.write("0WU,I=1,A=1,U=M,D=0,N=W,F=1\r\n")
-	time.Sleep(delay)
-	wxt.write("0WU,R=0100100001001000\r\n")
-	time.Sleep(delay)
-	wxt.write("0TU,I=1,P=H,T=F\r\n")
-	time.Sleep(delay)
-	wxt.write("0TU,R=1101000011010000\r\n")
-	time.Sleep(delay)
-	wxt.write("0SU,S=N,H=Y,I=5\r\n")
-	time.Sleep(delay)
-	wxt.write("0SU,R=1111000000000000\r\n")
-	time.Sleep(delay)
+	commands := []string{"0XU,M=P,C=3,B=4800,L=25\r\n", "0WU,I=1,A=1,U=M,D=0,N=W,F=1\r\n", "0WU,R=0100100001001000\r\n",
+		"0TU,I=1,P=H,T=F\r\n", "0TU,R=1101000011010000\r\n", "0SU,S=N,H=Y,I=5\r\n", "0SU,R=1111000000000000\r\n"}
+	for _, cmd := range commands {
+		time.Sleep(time.Millisecond * 100)
+		wxt.write(cmd)
+		time.Sleep(time.Millisecond * 100)
+	}
 	wxt.port.Flush()
 	wxt.Response.Time = time.Now()
 	wxt.write("0R0\r\n")
@@ -50,7 +40,7 @@ func (wxt *Device) Configure() {
 
 func (wxt *Device) UpdateResponse() {
 	resp := strings.Split(strings.TrimSpace(wxt.read()),",")
-	wxt.Response.Parse(resp[1:])
+	wxt.Response.Parse(resp)
 	wxt.Response.Time = time.Now()
 	wxt.write("0R0\r\n")
 }
