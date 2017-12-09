f51ef14800cfe2835730227e78c2354c5cdc7765
sharath <sharathramku@umass.edu>
2017-12-09 13:22:39 -0500

    non-blocking polling

--- main.go
+++ main.go
@@ -9,14 +9,29 @@ import (
 	"bufio"
 )
 
-func sampleW(x *wxt.Device) {
-	var index uint64 = 0
+type Sampler struct {
+	Writer *bufio.Writer
+	DataFile *os.File
+	Index uint64
+	Station *wxt.Device
+}
+
+func InitializeSampler(station *wxt.Device) *Sampler {
+	s := new(Sampler)
+	s.NewFile()
+	s.Index = 0
+	s. Station = station
+	return s
+}
+
+func (s *Sampler) NewFile() {
+	s.DataFile.Close()
 
 	folder := time.Now().Format("data-20060102")
-	filename := fmt.Sprintf("WX%d-%s",x.Id,time.Now().Format("20060102-030405.txt"))
+	filename := fmt.Sprintf("WX%d-%s",s.Station.Id,time.Now().Format("20060102-030405.txt"))
 	os.Mkdir(folder, 0777)
 	file, _ := os.Create(path.Join(folder, filename))
-	w := bufio.NewWriter(file)
+	s.Writer = bufio.NewWriter(file)
 
 	header := fmt.Sprintf("Model Number: WXT-510\n")
 	header += fmt.Sprintf("Sample rate: 1 (Hz)\n")
@@ -26,44 +41,35 @@ func sampleW(x *wxt.Device) {
 	header += fmt.Sprintf("Index, Hour, Minute, Second, Direction, Speed, Temp, Humidity, Pressure\n")
 	header += fmt.Sprintf("_______________________________________________________________________\n\n")
 	fmt.Print(header)
-	fmt.Fprint(w, header)
-	w.Flush()
+	fmt.Fprint(s.Writer, header)
+	s.Writer.Flush()
+}
 
-	for {
-		if time.Now().Second() != x.Response.Time.Second() {
-			if time.Now().Day() != x.Response.Time.Day() {
-				file.Close()
-				folder = time.Now().Format("data-20060102")
-				filename = fmt.Sprintf("WX%d-%s",x.Id,time.Now().Format("20060102-030405.txt"))
-				os.Mkdir(folder, 0777)
-				file, _ = os.Create(path.Join(folder, filename))
-				w = bufio.NewWriter(file)
-				fmt.Print(header)
-				fmt.Fprint(w, header)
-				w.Flush()
-			}
-			x.UpdateResponse()
-			line := fmt.Sprintf("%d, %s, %d, %.1f, %.1f, %.1f, %.1f\n",
-				index, x.Response.Time.Format("15, 04, 05.999999"),
-					x.Response.WindDir, x.Response.WindAvg,
-						x.Response.Temp, x.Response.Humidity,
-							x.Response.Pressure)
-			fmt.Print(line)
-			fmt.Fprint(w, line)
-			w.Flush()
-			index++
-		}
-		// need to fix with select{} eventually
-		time.Sleep(time.Millisecond*50)
+func (s *Sampler) poll() {
+	if time.Now().Day() != s.Station.Response.Time.Day() {
+		s.NewFile()
 	}
+	s.Station.UpdateResponse()
+	line := fmt.Sprintf("%d, %s, %d, %.1f, %.1f, %.1f, %.1f\n",
+		s.Index, s.Station.Response.Time.Format("15, 04, 05.999999"),
+		s.Station.Response.WindDir, s.Station.Response.WindAvg,
+		s.Station.Response.Temp, s.Station.Response.Humidity,
+		s.Station.Response.Pressure)
+	fmt.Print(line)
+	fmt.Fprint(s.Writer, line)
+	s.Writer.Flush()
+	s.Index++
 }
 
 
 func main() {
-	wStations := wxt.Load(os.Args[1])
-	for _, w := range wStations {
-		w.Configure()
-		go sampleW(&w)
+	station := wxt.Load(os.Args[1])[0]
+
+	station.Configure()
+	sampler := InitializeSampler(&station)
+
+	for range time.NewTicker(time.Second).C {
+		sampler.poll()
 	}
 	select {}
 }
